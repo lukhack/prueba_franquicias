95d037413ca875e934f61f1df1d9a88e162d106f
lukhack <juligrva@hotmail.com>
2025-01-12 17:25:05 -0500

    funcional programacion

---
+++ src/main/java/com/franquicias/franquicias/infrastruture/util/MessageSourceHelper.java
@@ -0,0 +1,31 @@
+package com.icodeap.ecommerce.infrastructure.util;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class MessageSourceHelper {
+
+    private final MessageSource messageSource;
+
+    public MessageSourceHelper(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    public String getMessage(String key) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(key, null, locale);
+    }
+
+    public String getMessage(String key, Object[] args) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(key, args, locale);
+    }
+
+    public String getMessage(String key, Object[] args, Locale locale) {
+        return messageSource.getMessage(key, args, locale);
+    }
+}
