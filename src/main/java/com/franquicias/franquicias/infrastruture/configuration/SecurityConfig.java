0047d6b4fa87abef7e0a61a4ad26480081071ced
lukhack <juligrva@hotmail.com>
2025-01-12 18:21:08 -0500

    reactive

--- src/main/java/com/franquicias/franquicias/infrastruture/configuration/SecurityConfig.java
+++ src/main/java/com/franquicias/franquicias/infrastruture/configuration/SecurityConfig.java
@@ -1,12 +1,8 @@
 package com.franquicias.franquicias.infrastruture.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
